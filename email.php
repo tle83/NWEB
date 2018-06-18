da60c9f19730069f3e6206bbc76e9138eb353f1c
tle83 <tnle833@gmail.com>
2018-06-18 10:27:57 -0500

    new documents
    -add in new project
    -add new contact me by email (not finish)
    -change home page for graduation date
    -deleted social media links

---
+++ email.php
@@ -0,0 +1,27 @@
+<?php
+$to = 'TNLE833@GMAIL.COM';
+$subject = test_input($_POST['name']);
+$header = test_input($_POST['email']);
+$message = $_POST['message'];
+$errorMessage = "";
+
+if(empty($subject)){
+    $errorMessage = "Name is required";
+}
+else{
+    if(!preg_match("/^[a-zA-Z ]*$/",$subject)){
+        $errorMessage = "Only letters and white space allowed";
+    }
+}
+
+if(empty($header)){
+    $errorMessage = "Return email is required";
+}
+else{
+    if (!filter_var($header, FILTER_VALIDATE_EMAIL)) {
+        $errorMessage = "Invalid email format";
+    }
+}
+
+mail($to, $subject, $message, $header);
+?>
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+//Transitioning through hmtl pages
 function linkPage(name) {
     window.location.assign(name);
 }
-
 window.onload = function () {
     var elements = document.getElementsByClassName('button');
     for (var i = 0; i < elements.length; i++) {
@@ -10,3 +10,26 @@ window.onload = function () {
         }
     }
 };
+
+// toggle modal for contact me in link.html
+var modal = document.getElementById('emailModal');
+var trigger = document.getElementById('modalTrigger');
+var xClose = document.getElementsByClassName('close')[0];
+var closeButton = document.getElementById('closeButton');
+//listen for click
+trigger.addEventListener('click', openModal);
+xClose.addEventListener('click', closeModal);
+closeButton.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+//functions
+function openModal(){
+    modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
+function outsideClick(event){
+    if(event.target == modal){
+        modal.style.display = 'none';
+    }
+}
